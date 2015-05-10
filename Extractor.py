2be8c479cc9626d58ab31222f532259bec826c55
cyy5121212 <515822895@qq.com>
2015-05-10 13:40:34 +0800

    do not develop in windows, many of problems occoured

--- Extractor.py
+++ Extractor.py
@@ -1,4 +1,20 @@
 __author__ = 'yiyuchen'
+import Logging
+import lxml.html as lht
+
+'''
+base ref: relative url convert to absolute address
+tags    : expected url tags
+'''
+def extract_links(html, base_ref, tags=[]):
+	if not html.strip():
+		return
+	link_list = []
+	try:
+		doc = H.document_fromstring(html)
+	except Exception, e:
+		Log
+	pass
 
 
 def extract(extract_queue):
